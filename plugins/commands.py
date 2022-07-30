fff16809f5571fc5e2bfd3b4457a5b63efb8fc02
Binshad1233 <108752527+Binshad1233@users.noreply.github.com>
2022-07-29 21:59:23 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('join', url='https://t.me/cinimabucketv1') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
